29595dd38e516c77cab723c1be4857681c5fb1c5
printMelen <alvaro.redondofgb@gmail.com>
2024-04-26 22:01:22 +0200

    Añadiendo tokens

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,16 +2,16 @@ const User = require('../model/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const CONTRASENIA_JWT = "nfsdfsañalmvdp221o3m33";
-const signUp = async(req,res,next)=>{
-    const {nombre,correo,contrasenia} = req.body;
+const signUp = async (req, res, next) => {
+    const { nombre, correo, contrasenia } = req.body;
     let userExist;
     try {
-        userExist = await User.findOne({correo:correo});
+        userExist = await User.findOne({ correo: correo });
     } catch (err) {
-      console.log(err);  
+        console.log(err);
     }
-    if(userExist){
-        return res.status(400).json({message: "El usuario ya esta registrado"});
+    if (userExist) {
+        return res.status(400).json({ message: "El usuario ya esta registrado" });
     }
     const contraseniaHasheada = bcrypt.hashSync(contrasenia);
     const user = new User({
@@ -22,35 +22,58 @@ const signUp = async(req,res,next)=>{
         rol: "user",
     });
 
-    try{    
+    try {
         await user.save();
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 
-    return res.status(201).json({message:user});
+    return res.status(201).json({ message: user });
 }
-const login = async(req,res,next)=>{
-    const {correo,contrasenia} = req.body;
+const login = async (req, res, next) => {
+    const { correo, contrasenia } = req.body;
     let userExist;
     try {
-        userExist = await User.findOne({correo:correo});
+        userExist = await User.findOne({ correo: correo });
     } catch (err) {
-      console.log(err);  
+        console.log(err);
     }
-    if(!userExist){
-        return res.status(400).json({message: "El usuario no existe registrese"});
+    if (!userExist) {
+        return res.status(400).json({ message: "El usuario no existe registrese" });
     }
-    const contraseniaCorrecta = bcrypt.compareSync(contrasenia,userExist.contrasenia);
+    const contraseniaCorrecta = bcrypt.compareSync(contrasenia, userExist.contrasenia);
     if (!contraseniaCorrecta) {
-        return res.status(400).json({message: "Datos incorrectos vuelva a intentarlo"});
+        return res.status(400).json({ message: "Datos incorrectos vuelva a intentarlo" });
     }
-    const token = jwt.sign({id: userExist._id},CONTRASENIA_JWT,{
+    const token = jwt.sign({ id: userExist._id }, CONTRASENIA_JWT, {
         expiresIn: '1h',
     })
-    return res.status(200).json({message:"Inicio de sesión correcto",
-    user: userExist , token
+    return res.status(200).json({
+        message: "Inicio de sesión correcto",
+        user: userExist, token
     });
 }
-exports.signUp= signUp;
-exports.login= login;
+
+const verifyToken = async (req, res, next) => {
+    const headers = req.headers['authorization'];
+    const token = headers.split(' ')[1];
+    if(!token){
+        return res.status(404).json({message:"Token no encontrado"});
+    }
+    jwt.verify(String(token),CONTRASENIA_JWT,(err,user)=>{
+        if(err){
+            return res.status(400).json({message:"Token invalido"});
+        }
+        console.log(user.id);
+        req.id = user.id;
+    });
+    next();
+};
+
+const getUser = async (req, res, next) => {
+
+};
+exports.signUp = signUp;
+exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signUp,login }= require('../controllers/user-controller');
+const { signUp,login, verifyToken }= require('../controllers/user-controller');
 
 const router = express.Router();
 
 router.post('/signup',signUp);
 router.post('/login',login);
+router.get('/user',verifyToken);
 
 module.exports = router;
